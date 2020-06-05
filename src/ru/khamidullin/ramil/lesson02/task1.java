04de8f9837e396033faf7b115600dcf3fd759143
Ramil <r.khamidullin.stc@innopolis.university>
2020-06-05 08:44:40 +0300

    Initial commit

---
+++ src/ru/khamidullin/ramil/lesson02/task1.java
@@ -0,0 +1,10 @@
+package ru.khamidullin.ramil.lesson02;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a = 30; //количество литров
+        int b = 42; //цена за 1 литр
+        int c = a*b; //стоимость бензина
+        System.out.println("Стоимость бензина равна " + c + " рублей");
+    }
+}
