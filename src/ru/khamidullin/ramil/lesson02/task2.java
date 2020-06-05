6ed27b52a7f3c6a152e5a65b383c4bfbf0c9760b
Ramil <r.khamidullin.stc@innopolis.university>
2020-06-05 09:53:24 +0300

    add task2 and task3

---
+++ src/ru/khamidullin/ramil/lesson02/task2.java
@@ -0,0 +1,9 @@
+package ru.khamidullin.ramil.lesson02;
+
+public class task2 {
+    public static void main(String[] args) {
+        int a = 45000; //величина зарплаты
+        double b = 0.87; //НДФЛ
+        System.out.println("Зарлата на руки равна " + a*b + " рублей");
+    }
+}
---
+++ src/ru/khamidullin/ramil/lesson02/task3.java
@@ -0,0 +1,10 @@
+package ru.khamidullin.ramil.lesson02;
+
+public class task3 {
+    public static void main(String[] args) {
+        int a = 3600; //количество секунд
+        int b = a/3600; //количество часов
+        System.out.println(b);
+
+    }
+}
