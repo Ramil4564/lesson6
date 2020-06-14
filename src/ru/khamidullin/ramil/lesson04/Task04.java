d48bdd78c58fae7e2869892728ea16fed1e1422d
Ramil <r.khamidullin.stc@innopolis.university>
2020-06-14 22:31:15 +0300

    +Task4

---
+++ src/ru/khamidullin/ramil/lesson04/Task04.java
@@ -0,0 +1,23 @@
+package ru.khamidullin.ramil.lesson04;
+
+public class Task04 { //арифметическая прогрессия
+    public static void main(String[] args) {
+
+        String type = args[0];
+
+        int firstTerm = Integer.parseInt(args[1]);
+
+        int difference = Integer.parseInt(args[2]);
+
+        int numberOfTerms = Integer.parseInt(args[3]);
+
+        System.out.print(firstTerm);
+
+        while (numberOfTerms-- > 1) {
+
+            System.out.printf(" %d", (firstTerm += difference));
+        }
+
+        System.out.println();
+    }
+}
---
+++ src/ru/khamidullin/ramil/lesson04/Task04_geom.java
@@ -0,0 +1,24 @@
+package ru.khamidullin.ramil.lesson04;
+
+public class Task04_geom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+
+        int firstTerm = scanner.nextInt();
+
+        int denominator = scanner.nextInt();
+
+        int numberOfTerms = scanner.nextInt();
+
+        System.out.print(firstTerm);
+
+        while (numberOfTerms-- > 1) {
+
+            System.out.printf(" %d", (firstTerm *= denominator));
+        }
+
+        System.out.println();
+    }
+}
